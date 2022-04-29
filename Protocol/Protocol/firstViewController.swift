54136310681d7d324b85208556794aa4e6126a15
Manav Shah <mvs6909@psu.edu>
2022-04-29 12:26:34 -0400

    Added the logic

--- Protocol/Protocol/firstViewController.swift
+++ Protocol/Protocol/firstViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class firstViewController: UIViewController {
+class firstViewController: UIViewController, sendData {
+    
+    
 
+    @IBOutlet weak var responseLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +20,20 @@ class firstViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        let secondView = segue.destination as! secondViewController
+        secondView.recievedText = textField.text! // reading data from the textField and giving it to the next view controller
+        secondView.delegate = self
+        
         // Pass the selected object to the new view controller.
     }
-    */
+    func send(message: String) {
+        responseLabel.text = message
+    }
 
 }
--- Protocol/Protocol/secondViewController.swift
+++ Protocol/Protocol/secondViewController.swift
@@ -7,23 +7,21 @@
 
 import UIKit
 
+
+protocol sendData{
+    func send(message: String)
+}
+
 class secondViewController: UIViewController {
+    
+    @IBOutlet weak var recievedLabel: UILabel!
+    var recievedText:String = "" //declaring a variable to recieve data
+    var delegate:sendData?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        recievedLabel.text = recievedText //giving data to the label
+        delegate?.send(message:String(recievedText.reversed()))
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
